fb394c66780ad3b2e8922163b367d136a02f760c
RafaTeron <rafael_sabreu@hotmail.com>
2024-10-27 16:08:20 -0300

    loginJWT endpoint validateToken/Gateway routes token

--- Login_JWT/src/main/java/com/rafaelAbreu/LoginJWT/resources/AuthenticationController.java
+++ Login_JWT/src/main/java/com/rafaelAbreu/LoginJWT/resources/AuthenticationController.java
@@ -5,8 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +41,11 @@ public class AuthenticationController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build(); 
         }
+    }
+    
+    @GetMapping("/validate-token")
+    public ResponseEntity<Boolean> validateToken(@RequestHeader("Authorization") String token) {
+        String login = tokenService.validateToken(token.replace("Bearer ", ""));
+        return ResponseEntity.ok(!login.isEmpty());
     }
 }
---
+++ gateway-service/src/main/java/com/rafaelTeron/gateway_service/config/GatewayConfig.java
@@ -0,0 +1,56 @@
+package com.rafaelTeron.gateway_service.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class GatewayConfig {
+
+    private final WebClient.Builder webClientBuilder;
+
+    public GatewayConfig(WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route("LoginJWT", r -> r.path("/auth/**")
+                .uri("http://localhost:8002"))
+            .route("app-quiz", r -> r.path("/app-quiz/**") 
+                .filters(f -> f.filter((exchange, chain) -> {
+
+                    String authToken = exchange.getRequest().getHeaders().getFirst("Authorization");
+                    return validateToken(authToken)
+                            .flatMap(isValid -> {
+                                if (isValid) {
+                                    return chain.filter(exchange); 
+                                } else {
+
+                                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                                    return exchange.getResponse().setComplete();
+                                }
+                            });
+                }))
+                .uri("http://localhost:8001")) 
+            .build();
+    }
+
+    private Mono<Boolean> validateToken(String token) {
+        if (token != null && token.startsWith("Bearer ")) {
+            return webClientBuilder.build()
+                .get()
+                .uri("http://localhost:8002/auth/validate-token")
+                .header("Authorization", token)
+                .retrieve()
+                .bodyToMono(Boolean.class);
+        }
+        return Mono.just(false);
+    }
+}
