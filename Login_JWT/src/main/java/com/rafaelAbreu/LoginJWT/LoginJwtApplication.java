b822b92865cf42a038c1660da6f2bf3ec0422ea0
RafaTeron <rafael_sabreu@hotmail.com>
2024-10-27 15:05:50 -0300

    loginJWT feign(2)

--- Login_JWT/src/main/java/com/rafaelAbreu/LoginJWT/LoginJwtApplication.java
+++ Login_JWT/src/main/java/com/rafaelAbreu/LoginJWT/LoginJwtApplication.java
@@ -3,9 +3,11 @@ package com.rafaelAbreu.LoginJWT;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class LoginJwtApplication {
 
 	public static void main(String[] args) {
