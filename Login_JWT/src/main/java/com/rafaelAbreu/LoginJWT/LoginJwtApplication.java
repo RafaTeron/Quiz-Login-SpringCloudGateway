92e031e7377d20af8f49bd1a5bf5b49c4761d575
RafaTeron <rafael_sabreu@hotmail.com>
2024-10-27 02:01:27 -0300

    config eureka

--- Login_JWT/src/main/java/com/rafaelAbreu/LoginJWT/LoginJwtApplication.java
+++ Login_JWT/src/main/java/com/rafaelAbreu/LoginJWT/LoginJwtApplication.java
@@ -2,8 +2,10 @@ package com.rafaelAbreu.LoginJWT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LoginJwtApplication {
 
 	public static void main(String[] args) {
